ca729a6bca5d099df850459dc28f642c59c69aa6
Sakhile Masoka <sakhile.harvey@gmail.com>
2020-06-12 10:50:12 +0200

    Check user path

--- packratt/directories.py
+++ packratt/directories.py
@@ -4,5 +4,6 @@ from appdirs import AppDirs
 _dirs = AppDirs("packratt")
 user_data_dir = _dirs.user_data_dir
 user_cache_dir = _dirs.user_cache_dir
+user_config_dir = _dir.user_config_dir
 
 del _dirs
--- packratt/registry.py
+++ packratt/registry.py
@@ -6,6 +6,7 @@ import yaml
 from jsonschema import validate
 
 import packratt
+from packratt.directories import user_config_dir
 
 SCHEMA_PATH = Path(Path(packratt.__file__).parent,
                    "conf", "registry-schema.yaml")
@@ -24,8 +25,14 @@ del SCHEMA_PATH
 def load_registry(filename=None):
 
     if filename is None:
+
         path = Path(Path(packratt.__file__).parent,
                     "conf", "registry.yaml")
+
+        user_path = Path(user_config_dir, "registry.yaml")
+
+        if user_path.is_file():
+            path = user_path
 
     with open(path, "r") as f:
         registry = yaml.safe_load(f)
